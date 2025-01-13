ae3f36c17b6e295cb42e7c55cfa78709ab77472d
NullDev (Shadow) <chris@nulldev.org>
2025-01-13 02:11:04 +0100

    add config system

---
+++ config/config.template.js
@@ -0,0 +1,9 @@
+export default {
+    discord: {
+        bot_token: "",
+        bot_owner_ids: [],
+        bot_status: "Watching over #1128",
+        total_shards: /** @type {number | "auto" | undefined} */ (1),
+        shards_per_cluster: /** @type {number | undefined} */ (1),
+    },
+};
