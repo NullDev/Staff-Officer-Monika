fcc555d384ae0d68b337ff52c484803d3a26c42c
NullDev (Shadow) <chris@nulldev.org>
2025-01-13 02:17:06 +0100

    add command translations

---
+++ locales/translations.js
@@ -0,0 +1,32 @@
+export default {
+    help: {
+        desc: "Show an overview of commands.",
+        translations: {
+            de: "Zeig eine Übersicht aller Befehle.",
+            fr: "Afficher un aperçu des commandes.",
+            ru: "Показать обзор команд.",
+            ja: "コマンドの概要を表示します。",
+            "es-ES": "Muestra una descripción general de los comandos.",
+        },
+    },
+    admin_help: {
+        desc: "Show an overview of all admin commands.",
+        translations: {
+            de: "Zeig eine Übersicht aller Admin-Befehle.",
+            fr: "Afficher un aperçu de toutes les commandes d'administration.",
+            ru: "Показать обзор всех административных команд.",
+            ja: "すべての管理者コマンドの概要を表示します。",
+            "es-ES": "Muestra una descripción general de todos los comandos de administración.",
+        },
+    },
+    info: {
+        desc: "Show information about this bot.",
+        translations: {
+            de: "Zeig Informationen über diesen Bot.",
+            fr: "Afficher des informations sur ce bot.",
+            ru: "Показать информацию об этом боте.",
+            ja: "このボットに関する情報を表示します。",
+            "es-ES": "Muestra información sobre este bot.",
+        },
+    },
+};
