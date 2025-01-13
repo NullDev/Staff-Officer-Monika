e63f368c396c28e82b5f7b33f5cece48b572e97b
NullDev (Shadow) <chris@nulldev.org>
2025-01-13 02:16:03 +0100

    add utils

---
+++ src/util/defaults.js
@@ -0,0 +1,9 @@
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+const defaults = {
+    info_embed_color: 2518621,
+};
+
+export default defaults;
