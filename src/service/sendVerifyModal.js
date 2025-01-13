e6d92bcef3414dc6ac2dccc1c075056a6bd3355d
NullDev (Shadow) <chris@nulldev.org>
2025-01-13 05:39:56 +0100

    fire modal

---
+++ src/service/sendVerifyModal.js
@@ -0,0 +1,33 @@
+import { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } from "discord.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+const sendVerifyModal = async function(interaction){
+    const modal = new ModalBuilder()
+        .setCustomId("submit_into")
+        .setTitle("Last War Information");
+
+    const username = new TextInputBuilder()
+        .setCustomId("username")
+        .setStyle(TextInputStyle.Short)
+        .setLabel("Your Last War Username")
+        .setRequired(true);
+
+    const clan = new TextInputBuilder()
+        .setCustomId("clan")
+        .setPlaceholder("TA5")
+        .setStyle(TextInputStyle.Paragraph)
+        .setLabel("Clan Tag (If no clan, type \"none\")")
+        .setRequired(true);
+
+    const first = /** @type {ActionRowBuilder<import("discord.js").ModalActionRowComponentBuilder>} */ (new ActionRowBuilder()).addComponents(username);
+    const second = /** @type {ActionRowBuilder<import("discord.js").ModalActionRowComponentBuilder>} */ (new ActionRowBuilder()).addComponents(clan);
+
+    modal.addComponents(first, second);
+
+    return await interaction.showModal(modal);
+};
+
+export default sendVerifyModal;
